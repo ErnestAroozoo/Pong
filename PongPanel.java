c0526a50e15ed028e5f54408082f8da583b10433
ErnestAroozoo <ernestaroozoo@gmail.com>
2018-12-07 10:19:30 -0500

    Uploaded Basic Pong Code

---
+++ PongPanel.java
@@ -0,0 +1,121 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class PongPanel extends JPanel{
+	// Properties
+	int intPaddleY1 = 300;
+	int intPaddleY2 = 300;
+	int intBallX = 640;
+	int intBallY = 360;
+	int intScore1 = 0;
+	int intScore2 = 0;
+	boolean blnPaddle1Down;
+	boolean blnPaddle1Up;
+	boolean blnPaddle2Down;
+	boolean blnPaddle2Up;
+	boolean blnBallRight = false;
+	boolean blnBallLeft = true;
+	boolean blnBallUp = false;
+	boolean blnBallDown = true;
+	
+	// Methods
+	public void paintComponent(Graphics g){
+		// Set Background
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 1280, 720); 
+		
+		// Paddle 1 
+		g.setColor(Color.WHITE);
+		g.fillRect(5, intPaddleY1, 10, 100 ); 
+		if(blnPaddle1Down){ // Translation movement of Paddle 1 using Boolean.
+			intPaddleY1 = intPaddleY1 + 20;
+		}
+		if(blnPaddle1Up){
+			intPaddleY1 = intPaddleY1 - 20;
+		}
+		if(intPaddleY1 < 0){ // Restrict Paddle1 Movement
+			intPaddleY1 = intPaddleY1 + 20;
+		}
+		if(intPaddleY1 > 620){
+			intPaddleY1 = intPaddleY1 - 20;
+		}
+		
+		// Paddle 2
+		g.setColor(Color.WHITE);
+		g.fillRect(1265, intPaddleY2, 10, 100); 
+		if(blnPaddle2Down){ // Translation movement of Paddle 2 using Boolean.
+			intPaddleY2 = intPaddleY2 + 20;
+		}
+		if(blnPaddle2Up){
+			intPaddleY2 = intPaddleY2 - 20;
+		}
+		if(intPaddleY2 < 0){
+			intPaddleY2 = 0;
+		}
+		if(intPaddleY2 > 620){
+			intPaddleY2 = 620;
+		}
+		
+		// Ball
+		g.setColor(Color.WHITE);
+		g.fillOval(intBallX, intBallY, 20, 20);
+		if(blnBallLeft == true){ // Ball Left
+			intBallX = intBallX - 5;
+		}
+		if(blnBallRight == true){ // Ball Right
+			intBallX = intBallX + 5;
+		}
+		if(blnBallUp == true){ // Ball Up
+			intBallY = intBallY - 5;
+		}
+		if(blnBallDown == true){ // Ball Down
+			intBallY = intBallY + 5;
+		}
+		
+		if(intBallX < 0){ // Left Wall - End Game
+			intScore1 = intScore1 + 1;
+			intBallX = 640;
+			intBallY = 360;
+			blnBallRight = false;
+			blnBallLeft = true;
+			blnBallUp = false;
+			blnBallDown = true;
+		}
+		if(intBallX > 1280){ // Right Wall - End Game
+			intScore2 = intScore2 + 1;
+			intBallX = 640;
+			intBallY = 360;
+			blnBallRight = false;
+			blnBallLeft = true;
+			blnBallUp = false;
+			blnBallDown = true;
+		}
+		if(intBallY == 0){ // Top Wall
+			blnBallUp = false;
+			blnBallDown = true;
+		}
+		if(intBallY == 700){ // Bottom Wall
+			blnBallDown = false;
+			blnBallUp = true;
+		}
+		if(intBallY >= intPaddleY1 && intBallY <= intPaddleY1 + 100 && intBallX == 15){ // Paddle 1
+			blnBallLeft = false;
+			blnBallRight = true;
+		}
+		if(intBallY >= intPaddleY2 && intBallY <= intPaddleY2 + 100 && intBallX == 1255){ // Paddle 2
+			blnBallRight = false;
+			blnBallLeft = true;
+		}
+	}
+	
+	// Constructors
+	public PongPanel(){
+		super();
+	}
+	
+	
+}
+
---
+++ PongProgram.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PongProgram implements ActionListener, KeyListener, MouseMotionListener{
+	// Properties
+	JFrame theframe;
+	PongPanel thepanel;
+	Timer thetimer;
+	JLabel score1;
+	JLabel score2;
+	
+	// Methods
+	public void actionPerformed(ActionEvent evt){ 
+		if(evt.getSource() == thetimer){ // If timer is triggered, it will repaint.
+				thepanel.repaint();
+		}
+	}
+	
+	public void keyReleased(KeyEvent evt){ // keyReleased is triggered when you release the key.
+		if(evt.getKeyCode() == 38){ // Up Arrow Key
+			thepanel.blnPaddle1Up = false;
+		}
+		else if(evt.getKeyCode() == 40){ // Down Arrow Key
+			thepanel.blnPaddle1Down = false;
+		}
+	}
+	public void keyPressed(KeyEvent evt){ // keyPressed is triggered when you press the key.
+		if(evt.getKeyCode() == 38){ // Up Arrow Key
+			thepanel.blnPaddle1Up = true;
+		}
+		else if(evt.getKeyCode() == 40){ // Down Arrow Key
+			thepanel.blnPaddle1Down = true;
+		}
+	}
+	public void keyTyped(KeyEvent evt){ // keyTyped is triggered when you press and release the key. (Note: Laggy because it's typed)
+		//
+	}
+	
+	public void mouseMoved(MouseEvent evt){
+		thepanel.intPaddleY2 = evt.getY(); // Set object Y-Coordinate to Mouse Y-Coordinate
+	}
+	public void mouseDragged(MouseEvent evt){
+		// 
+	}
+	
+	// Constructors
+	public PongProgram(){
+		theframe = new JFrame("Pong");
+		thepanel = new PongPanel();
+		thepanel.setLayout(null);
+		thepanel.setPreferredSize(new Dimension(1280, 720));
+		thepanel.addMouseMotionListener(this); // Add MouseMotionListener
+		
+		theframe.addKeyListener(this); // Add KeyListener
+		theframe.setContentPane(thepanel);
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theframe.pack();
+		theframe.setVisible(true);
+		
+		// Timer Object
+		thetimer = new Timer(1000/60, this); // Triggering timer every 1000/60. Basically 60 FPS.
+		thetimer.start();
+		
+		// Label Object
+		 score1 = new JLabel("0"); // Initilize the JLabel String to "0"
+		 score1.setSize(100, 100);
+		 score1.setLocation(500, 150);
+	
+		 score2 = new JLabel("0"); // Initilize the JLabel String to "0"
+		 score1.setSize(100, 100);
+		 score1.setLocation(780, 150);
+	
+		 
+	}
+	
+	// Main Methods
+	public static void main(String[] args){
+		new PongProgram();
+	}
+}
+
